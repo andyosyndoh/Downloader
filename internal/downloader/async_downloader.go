a37e4e9fd2ebfdd2a4707c77c8083326a494d78d
rcaleb <calebmadara58@gmail.com>
2024-08-20 13:17:24 +0300

    Refactor Downloader Package for Simplified URL Handling and Error Reporting
    - Removed the global `processedURLs` map and associated mutex to simplify code and focus on core download functionality.
    - Refactored `AsyncDownload` to use `http.NewRequest` with custom headers, ensuring compatibility with various download sources.
    - Updated the `Referer` header to use a more appropriate default value for HTTP requests.
    - Simplified the URL processing logic, removing the previous URL parsing and directory creation based on path components.
    - Modified file path handling to ensure consistent directory structure creation and file naming.
    - Added colorized output for download completion status using ANSI escape codes.
    - Streamlined error handling and logging for improved readability and maintainability.

--- internal/downloader/async_downloader.go
+++ internal/downloader/async_downloader.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,14 +12,6 @@ import (
 
 	"downloaderex/internal/rateLimiter"
 )
-
-// Global map to keep track of processed URLs
-var processedURLs = struct {
-	sync.Mutex
-	urls map[string]bool
-}{
-	urls: make(map[string]bool),
-}
 
 func DownloadMultipleFiles(filePath, outputFile, limit, directory string) {
 	file, err := os.Open(filePath)
@@ -34,6 +25,7 @@ func DownloadMultipleFiles(filePath, outputFile, limit, directory string) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		url := strings.TrimSpace(scanner.Text())
+
 		if url == "" {
 			continue // Skip empty lines
 		}
@@ -45,71 +37,43 @@ func DownloadMultipleFiles(filePath, outputFile, limit, directory string) {
 	}
 	wg.Wait()
 }
-
-func AsyncDownload(outputFileName, urlStr, limit, directory string) {
-	// Check if the URL has already been processed
-	processedURLs.Lock()
-	if processed, exists := processedURLs.urls[urlStr]; exists && processed {
-		processedURLs.Unlock()
-		fmt.Printf("URL already processed: %s\n", urlStr)
-		return
-	}
-	processedURLs.Unlock()
-
+func AsyncDownload(outputFileName, url, limit, directory string) {
+	path := ExpandPath(directory)
 	// startTime := time.Now()
 	// fmt.Printf("Start at %s\n", startTime.Format("2006-01-02 15:04:05"))
 
-	// Parse the URL to get the path components
-	u, err := url.Parse(urlStr)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Error parsing URL:", err)
+		fmt.Println("Error creating request:", err)
 		return
 	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+	req.Header.Set("Referer", "http://ipv4.download.thinkbroadband.com/") // Change to a referer appropriate for the URL
 
-	// Create the necessary directories based on the URL path
-	rootPath := ExpandPath(directory)
-	pathComponents := strings.Split(strings.Trim(u.Path, "/"), "/")
-	relativeDirPath := filepath.Join(pathComponents[:len(pathComponents)-1]...)
-	fullDirPath := filepath.Join(rootPath, relativeDirPath)
-	fileName := pathComponents[len(pathComponents)-1]
-
-	resp, err := http.Get(urlStr)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println("Error downloading file:", err)
 		return
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error: status %s\n", resp.Status)
+		fmt.Printf("Error: status %s url: [%s]\n", resp.Status, url)
 		return
 	}
 
-	contentType := resp.Header.Get("Content-Type")
-	// contentLength := resp.ContentLength
-	// fmt.Printf("Content size: %d bytes [~%.2fMB]\n", contentLength, float64(contentLength)/1024/1024)
-
 	if outputFileName == "" {
-		if fileName == "" || strings.HasSuffix(urlStr, "/") {
-			fileName = "index.html"
-		} else if contentType == "text/html" && !strings.HasSuffix(fileName, ".html") {
-			fileName += ".html"
-		}
-		outputFileName = filepath.Join(fullDirPath, fileName)
+		urlParts := strings.Split(url, "/")
+		fileName := urlParts[len(urlParts)-1]
+		outputFileName = filepath.Join(path, fileName)
 	} else {
-		if contentType == "text/html" && !strings.HasSuffix(outputFileName, ".html") {
-			outputFileName += ".html"
-		}
-		outputFileName = filepath.Join(fullDirPath, outputFileName)
+		outputFileName = filepath.Join(path, outputFileName)
 	}
 
-	if fullDirPath != "" {
-		if _, err := os.Stat(fullDirPath); os.IsNotExist(err) {
-			err = os.MkdirAll(fullDirPath, 0o755)
-			if err != nil {
-				fmt.Println("Error creating path:", err)
-				return
-			}
+	if path != "" {
+		err = os.MkdirAll(path, 0o755)
+		if err != nil {
+			fmt.Println("Error creating directory:", err)
+			return
 		}
 	}
 
@@ -148,14 +112,7 @@ func AsyncDownload(outputFileName, urlStr, limit, directory string) {
 		}
 	}
 
-	fmt.Println() // Move to the next line after download completes
-
 	// endTime := time.Now()
-	fmt.Printf("Downloaded [%s]\n", urlStr)
+	fmt.Printf("\033[32mDownloaded\033[0m [%s]\n", url)
 	// fmt.Printf("Finished at %s\n", endTime.Format("2006-01-02 15:04:05"))
-
-	// Mark the URL as processed
-	processedURLs.Lock()
-	processedURLs.urls[urlStr] = true
-	processedURLs.Unlock()
 }
