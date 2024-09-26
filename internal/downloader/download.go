30047b53fdaedf443fbd1fb8f75e3e2a9941bf2e
aosindo <andyovvo8@gmail.com>
2024-09-26 15:09:59 +0300

    fix: removed unnesesary code

--- internal/downloader/download.go
+++ internal/downloader/download.go
@@ -59,7 +59,7 @@ func OneDownload(file, url, limit, directory string) {
 	}
 	temp := ""
 	if file != "" && directory != "" {
-		file = "/" + file
+		file =  file
 		fmt.Printf("saving file to: %s%s\n", directory, file)
 	} else if path == "" && file != "" {
 		temp = "./"
